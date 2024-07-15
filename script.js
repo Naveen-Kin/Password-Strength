57e86f481625e997d2f6e5b69fa412c6d823613b
Naveen-Kin <gnaveen042000@gmail.com>
2024-07-15 22:28:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var pass = document.getElementById ("password");
+var msg = document.getElementById ("message");
+var str = document.getElementById ("strenght");
+
+pass.addEventListener ('input', ()=>{
+    if(pass.value.length > 0) {
+        msg.style.display ="block";
+    }
+    else {
+        msg.style.display ="none";
+    }
+     if (pass.value.length < 6){
+        str.innerHTML = "week";
+    }
+
+        else if (pass.value.length >= 6 && pass.value.length < 8){
+            str.innerHTML = "medium";
+        }
+        else if (pass.value.length >= 8){
+            str.innerHTML = "strong";
+        }
+});
